f04dad9adf87767ff52f18584404f989d7afceb3
hab278 <hab278@nyu.edu>
2014-07-14 14:21:46 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,11 @@
 require 'capistrano/ext/multistage'
 
 set :ssh_options, {:forward_agent => true}
-set :application, "citationPushToPlugin"
+set :application, "citeroPushToPlugin"
 # Your Primo user
-set :user, ""
+set :user, ENV['USER']
 # Your javacompiler
-set :javac, ""
+set :javac, ENV['JAVAC']
 
 default_run_options[:shell] = '/bin/bash --login'
 
@@ -83,11 +83,10 @@ namespace :deploy do
     run "cp -f #{deploy_to}/library/CitationProcess.class /exlibris/primo/p4_1/ng/primo/home/system/tomcat/search/webapps/primo_library#libweb/WEB-INF/classes/edu/nyu/library/"
 
     run "mv #{deploy_to}/library/CitationProcess.java #{deploy_to}"
-    #run "rm -rf #{deploy_to}/library"
-    #run "rm -rf #{deploy_to}/releases"
-    #run "rm -rf #{deploy_to}/shared"
+    run "rm -rf #{deploy_to}/library"
+    run "rm -rf #{deploy_to}/releases"
+    run "rm -rf #{deploy_to}/shared"
     
-    run "cat #{deploy_to}/library/CitationProcess.java"
     run "echo \"All done\""
     cleanup
   end
