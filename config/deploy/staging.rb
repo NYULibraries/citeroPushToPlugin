6bb3a192493935ca3371ce3d2e1adf0a7c4fdb09
hab278 <hab278@nyu.edu>
2014-07-14 14:33:23 -0400

    Update staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 server "primodev1.bobst.nyu.edu", :app, :web
 server "primodev2.bobst.nyu.edu", :app, :web
-set(:branch, 'devel') unless exists?(:branch)
+set(:branch, 'master') unless exists?(:branch)
