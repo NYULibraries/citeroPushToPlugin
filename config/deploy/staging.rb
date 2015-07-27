e74e1f0deb120f3c99c7c857412d7156d86174d8
hab278 <hab278@nyu.edu>
2015-07-27 13:51:16 -0400

    Use environmental branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,4 +3,4 @@ Figs.load(stage: "staging")
 Figs.env.deploy_servers.each do |srvr|
   server srvr, :app, :web
 end
-set(:branch, 'master') unless exists?(:branch)
+set(:branch, ENV["GIT_BRANCH"].gsub(/remotes\//,"").gsub(/origin\//,""))
